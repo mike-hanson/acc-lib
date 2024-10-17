813733bba903b5b5e103da325ff64c2141d81a27
Mike Hanson <mike@mikehanson.com>
2024-10-17 07:58:23 +0100

    Add user control for rendering code examples

---
+++ Acc.Lib.Gallery/Controls/CodeBlockLanguage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Acc.Lib.Gallery.Controls;
+
+public enum CodeBlockLanguage
+{
+    Asax,
+    Ashx,
+    Aspx,
+    AspxCs,
+    AspxVb,
+    CSharp,
+    Cpp,
+    Css,
+    Fortran,
+    FSharp,
+    Haskell,
+    Html,
+    Java,
+    JavaScript,
+    Json,
+    Koka,
+    Markdown,
+    MatLab,
+    Php,
+    PowerShell,
+    Python,
+    Sql,
+    TypeScript,
+    VbDotNet,
+    Xaml,
+    Xml
+}
