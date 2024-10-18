ca2632aeca33ce25b877c264996fc82bfb5fdb6d
Mike Hanson <mike@mikehanson.com>
2024-10-18 05:47:03 +0100

    Abandon idea of gallery in favour of separate sample apps for broadcasting and shared memory

--- Acc.Lib.Gallery/ViewModels/Pages/AccDataProviderPageViewModel.cs
+++ Acc.Lib.Gallery/ViewModels/Pages/AccDataProviderPageViewModel.cs
@@ -2,4 +2,13 @@
 
 namespace Acc.Lib.Gallery.ViewModels.Pages;
 
-public class AccDataProviderPageViewModel : ObservableObject { }
+public class AccDataProviderPageViewModel : ObservableObject
+{
+    public string GetCustomCarsSignature { get; } =
+        @"public static IEnumerable<CustomCar> GetCustomCars();";
+    public string GetCustomCarsUsage { get; } = @"var cars = AccDataProvider.GetCustomCars();
+foreach(var car in cars) {
+    // do something with each car
+}";
+
+}
