0e7848c7336190ef49827e90dc5c212ffe484ca6
Mike Hanson <mike@mikehanson.com>
2024-12-13 09:03:27 +0000

    Add gap time extension methods

--- Acc.Lib.Core/Constants.cs
+++ Acc.Lib.Core/Constants.cs
@@ -6,4 +6,11 @@ public static class Constants
 {
     public const string TimingFormat = "mm\\:ss\\.fff";
     public const string TimeFormat = "hh\\:mm";
+    public const string SecondsGapFormat = "s\\.fff";
+    public const string MinutesGapFormat = "m\\:ss\\.fff";
+    public const string HoursGapFormat = "hh\\:mm";
+
+    public const int MillisecondsPerSecond = 1000;
+    public const int MilliSecondsPerMinute = 60 * 1000;
+    public const int MillisecondsPerHour = 60 * (60 * 1000);
 }
--- Acc.Lib.Core/Extensions.cs
+++ Acc.Lib.Core/Extensions.cs
@@ -8,37 +8,77 @@ public static class Extensions
     public static string ToFriendlyName(this RaceSessionType sessionType)
     {
         return sessionType switch
-        {
-            RaceSessionType.HotlapSuperpole => "Hotlap Superpole"
-                 ,
-            _ => sessionType.ToString()
-        };
+               {
+                   RaceSessionType.HotlapSuperpole => "Hotlap Superpole",
+                   _ => sessionType.ToString()
+               };
     }
 
     public static string ToFriendlyName(this SessionPhase sessionType)
     {
         return sessionType switch
-        {
-            SessionPhase.FormationLap => "Formation Lap",
-            SessionPhase.PostSession => "Post Session",
-            SessionPhase.PreFormation => "Pre Formation",
-            SessionPhase.PreSession => "Pre Session",
-            SessionPhase.SessionOver => "Session Over",
-            _ => sessionType.ToString()
-        };
+               {
+                   SessionPhase.FormationLap => "Formation Lap",
+                   SessionPhase.PostSession => "Post Session",
+                   SessionPhase.PreFormation => "Pre Formation",
+                   SessionPhase.PreSession => "Pre Session",
+                   SessionPhase.SessionOver => "Session Over",
+                   _ => sessionType.ToString()
+               };
     }
 
-    public static long ValidatedValue(this long longValue)
+    public static string ToGapString(this int timingMs)
     {
-        return longValue >= int.MaxValue ? 0 : longValue;
+        var timeSpan = TimeSpan.FromMilliseconds(timingMs);
+
+        return timingMs switch
+               {
+                   >= Constants.MillisecondsPerHour => timeSpan.ToString(Constants.HoursGapFormat),
+                   >= Constants.MilliSecondsPerMinute => timeSpan.ToString(Constants
+                       .MinutesGapFormat),
+                   _ => timeSpan.ToString(Constants.SecondsGapFormat)
+               };
     }
 
-    public static int ValidatedValue(this int intValue)
+    public static string ToGapString(this long timingMs)
     {
-        return intValue >= int.MaxValue ? 0 : intValue;
+        var timeSpan = TimeSpan.FromMilliseconds(timingMs);
+
+        return timingMs switch
+               {
+                   >= Constants.MillisecondsPerHour => timeSpan.ToString(Constants.HoursGapFormat),
+                   >= Constants.MilliSecondsPerMinute => timeSpan.ToString(Constants
+                       .MinutesGapFormat),
+                   _ => timeSpan.ToString(Constants.SecondsGapFormat)
+               };
+    }
+
+    public static string ToGapString(this float timingMs)
+    {
+        var timeSpan = TimeSpan.FromMilliseconds(timingMs);
+
+        return timingMs switch
+               {
+                   >= Constants.MillisecondsPerHour => timeSpan.ToString(Constants.HoursGapFormat),
+                   >= Constants.MilliSecondsPerMinute => timeSpan.ToString(Constants
+                       .MinutesGapFormat),
+                   _ => timeSpan.ToString(Constants.SecondsGapFormat)
+               };
     }
 
     public static string ToTimeString(this double timeMs)
+    {
+        return TimeSpan.FromMilliseconds(timeMs)
+                       .ToString(Constants.TimeFormat);
+    }
+
+    public static string ToTimeString(this int timeMs)
+    {
+        return TimeSpan.FromMilliseconds(timeMs)
+                       .ToString(Constants.TimeFormat);
+    }
+
+    public static string ToTimeString(this long timeMs)
     {
         return TimeSpan.FromMilliseconds(timeMs)
                        .ToString(Constants.TimeFormat);
@@ -56,27 +96,25 @@ public static class Extensions
                        .ToString(Constants.TimingFormat);
     }
 
-    public static string ToTimeString(this int timeMs)
-    {
-        return TimeSpan.FromMilliseconds(timeMs)
-                       .ToString(Constants.TimeFormat);
-    }
-
     public static string ToTimingString(this int timingMs)
     {
         return TimeSpan.FromMilliseconds(timingMs)
                        .ToString(Constants.TimingFormat);
     }
 
-    public static string ToTimeString(this long timeMs)
-    {
-        return TimeSpan.FromMilliseconds(timeMs)
-                       .ToString(Constants.TimeFormat);
-    }
-
     public static string ToTimingString(this long timingMs)
     {
         return TimeSpan.FromMilliseconds(timingMs)
                        .ToString(Constants.TimingFormat);
+    }
+
+    public static long ValidatedValue(this long longValue)
+    {
+        return longValue >= int.MaxValue? 0: longValue;
+    }
+
+    public static int ValidatedValue(this int intValue)
+    {
+        return intValue >= int.MaxValue? 0: intValue;
     }
 }
